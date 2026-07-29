7d2527626988016635e363099293c7a8406ad252
its-YogeshChandra <pandittheroyal@gmail.com>
2026-07-29 15:49:46 +0530

    feat: implement gRPC server initialization for storage node in main.rs

--- storagenode/src/main.rs
+++ storagenode/src/main.rs
@@ -2,5 +2,17 @@ mod server;
 pub mod storage_proto {
     tonic::include_proto!("storage_system");
 }
+use tonic::transport::Server;
+use tokio;
 
-fn main() {}
+#[tokio::main]
+async fn main( 
+    
+) -> Result<(), Box<dyn std::error::Error>> {
+     let addr = "[::1]:50051".parse()?;
+    Server::builder()
+    .add_service(storage_proto::grid_rock_server::GridRockServer::new(server::StorageServer))
+    .serve(addr).await?;
+    
+    Ok(())
+}
