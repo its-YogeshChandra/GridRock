7fa80f38cd43f5e452706aa61e328dd221befaf8
its-YogeshChandra <pandittheroyal@gmail.com>
2026-07-29 15:52:09 +0530

    feat: add server startup log message to main.rs

--- storagenode/src/main.rs
+++ storagenode/src/main.rs
@@ -10,6 +10,8 @@ async fn main(
     
 ) -> Result<(), Box<dyn std::error::Error>> {
      let addr = "[::1]:50051".parse()?;
+     println!("server is listening on the port 50051");
+    
     Server::builder()
     .add_service(storage_proto::grid_rock_server::GridRockServer::new(server::StorageServer))
     .serve(addr).await?;
