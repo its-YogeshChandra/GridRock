8ecb7e6979bc2cbf2142c5470fe7f8acf2c977b5
its-YogeshChandra <pandittheroyal@gmail.com>
2026-07-29 22:53:40 +0530

    feat: integrate raft-rs library and slog for storage node configuration and logging

--- storagenode/src/main.rs
+++ storagenode/src/main.rs
@@ -6,6 +6,12 @@ pub mod storage_proto {
 }
 use tonic::transport::Server;
 use tokio;
+use raft::{
+    Config, 
+    storage::MemStorage,
+    raw_node::RawNode,
+};
+use slog::{Drain, o};
 
 #[tokio::main]
 async fn main( 
@@ -13,7 +19,27 @@ async fn main(
 ) -> Result<(), Box<dyn std::error::Error>> {
      let addr = "[::1]:50051".parse()?;
      println!("server is listening on the port 50051");
-    
+
+     //create the config for creating raft node
+     //question : what config do in the first place  
+     let mut config = Config{
+        id: 1,
+        ..Default::default()
+     };
+
+     config.id = 3;
+
+   //initialize logger 
+   let logger = slog::Logger::root(slog);
+
+
+   //question : what this .validate is validating 
+   //and again what this is validating the config ? 
+   config.validate().unwrap();
+
+   let storage = MemStorage::new_with_conf_state((vec![1], vec![]));
+   let mut node = RawNode::new(&config, node storage).unwrap();
+
     Server::builder()
     .add_service(storage_proto::grid_rock_server::GridRockServer::new(server::StorageServer))
     .serve(addr).await?;
