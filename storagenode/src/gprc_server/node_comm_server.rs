fdea61a1b8e781027f1dd8e1f62342e8c3c0e48b
its-YogeshChandra <pandittheroyal@gmail.com>
2026-08-06 00:15:48 +0530

    feat: implement gRPC handler logic for Raft messaging, cluster proposals, info retrieval, and node joining

--- storagenode/src/gprc_server/node_comm_server.rs
+++ storagenode/src/gprc_server/node_comm_server.rs
@@ -12,21 +12,46 @@ pub struct NodeCoommServer{
 #[tonic::async_trait]
 impl NodeComm for NodeCoommServer {
  
-  async fn send_raft_message(&self, request: Request<RaftMessageRequest>) -> Result<Response<ForwardProposalRequest>, Status> {
+  async fn send_raft_message(&self, request: Request<RaftMessageRequest>) -> Result<Response<RaftMessageResponse>, Status> {
 
- } 
+    let buffer : Vec<u8> = vec![]; 
+    //create the response 
+    let response = RaftMessageResponse {
+        success : true
+    };
 
+    Ok(Response::new(response))
+ }
 
   async fn forward_proposal(&self , request: Request<ForwardProposalRequest> ) -> Result<Response<ForwardProposalResponse>, Status> {
 
- }
+    let response = ForwardProposalResponse {
+        success : true,
+        message : "Proposal forwarded successfully".to_string()
+    };
+
+    Ok(Response::new(response))
+  }
 
  async  fn get_cluster_info (&self , request: Request<GetClusterInfoRequest> ) -> Result<Response<GetClusterInfoResponse>, Status> {
-
+ 
+ let response = GetClusterInfoResponse {
+    leader_id : 0,
+    peers : vec![]
+ };
+ 
+ Ok(Response::new(response))   
+ 
  }
 
  async fn join_cluster(&self, request: Request<JoinClusterRequest>)-> Result<Response<JoinClusterResponse>, Status> {
+  let response = JoinClusterResponse {
+    success : true,
+    message : "Node joined successfully".to_string(),
+    peers : vec![]
+  };
 
+  Ok(Response::new(response))
  }  
 
 
