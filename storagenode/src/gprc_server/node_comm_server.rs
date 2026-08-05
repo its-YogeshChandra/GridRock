24fd200c8eb7e21015bc9de8467e974bdcb158a7
its-YogeshChandra <pandittheroyal@gmail.com>
2026-08-06 00:00:54 +0530

    feat: update NodeComm trait methods and fix get_cluster_info function signature

--- storagenode/src/gprc_server/node_comm_server.rs
+++ storagenode/src/gprc_server/node_comm_server.rs
@@ -1,18 +1,18 @@
 //gprc server side functions for node to node communication
 use prost::Message;
 use tonic::{Request, Response, Status};
-use crate::node_comm::{ForwardProposalRequest, ForwardProposalResponse, JoinClusterRequest, JoinClusterResponse, RaftMessageRequest, RaftMessageResponse, node_comm_server::NodeComm};  
+use crate::node_comm::{ForwardProposalRequest, ForwardProposalResponse, GetClusterInfoRequest, GetClusterInfoResponse, JoinClusterRequest, JoinClusterResponse, RaftMessageRequest, RaftMessageResponse, node_comm_server::NodeComm};  
 use tokio::sync::{mpsc::{Sender}, oneshot};
-
+use tokio;
 
 pub struct NodeCoommServer{
     pub tx: Sender<crate::node::node_utils::Msg>
 }
 
-#[tokio::async_trait]
+#[tonic::async_trait]
 impl NodeComm for NodeCoommServer {
  
-  async fn send_raft_message(&self, request: Request<RaftMessageRequest>) -> Result<Response<ForwardProposalRequest>, Status>{
+  async fn send_raft_message(&self, request: Request<RaftMessageRequest>) -> Result<Response<ForwardProposalRequest>, Status> {
 
  } 
 
@@ -21,7 +21,7 @@ impl NodeComm for NodeCoommServer {
 
  }
 
- async  fn get_cluseter_info(&self , request: Request<JoinClusterRequest> ) -> Result<Response<JoinClusterResponse>, Status> {
+ async  fn get_cluster_info (&self , request: Request<GetClusterInfoRequest> ) -> Result<Response<GetClusterInfoResponse>, Status> {
 
  }
 
