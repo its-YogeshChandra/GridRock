4e10938f9f8f81dec25395e5eaf783a3df7dd0e8
its-YogeshChandra <pandittheroyal@gmail.com>
2026-08-05 20:02:24 +0530

    feat: refactor build_storage_proto function and add RaftProposal message for operation handling

--- storagenode/build.rs
+++ storagenode/build.rs
@@ -1,5 +1,10 @@
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+pub fn build_storage_proto() -> Result<(), Box<dyn std::error::Error>> {
     tonic_prost_build::compile_protos("src/proto/storage.proto")?;
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    build_storage_proto()?;
     Ok(())
 }
