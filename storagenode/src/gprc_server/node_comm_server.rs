ccd23ae6d9653907e879a1c23af151513d7206e8
its-YogeshChandra <pandittheroyal@gmail.com>
2026-08-08 00:49:24 +0530

    feat: integrate protobuf and implement message deserialization for raft communication in gRPC server

--- storagenode/src/gprc_server/node_comm_server.rs
+++ storagenode/src/gprc_server/node_comm_server.rs
@@ -1,9 +1,10 @@
 //gprc server side functions for node to node communication
-use prost::Message;
 use tonic::{Request, Response, Status};
 use crate::node_comm::{ForwardProposalRequest, ForwardProposalResponse, GetClusterInfoRequest, GetClusterInfoResponse, JoinClusterRequest, JoinClusterResponse, RaftMessageRequest, RaftMessageResponse, node_comm_server::NodeComm};  
 use tokio::sync::{mpsc::{Sender}, oneshot};
 use tokio;
+use protobuf::Message as ProtobufMessage;
+use crate::node::node_utils::{Msg, ProposeMessage};
 
 pub struct NodeCommServer{
     pub tx: Sender<crate::node::node_utils::Msg>
@@ -11,12 +12,20 @@ pub struct NodeCommServer{
 
 #[tonic::async_trait]
 impl NodeComm for NodeCommServer {
- 
+
+  //sending message to raft 
   async fn send_raft_message(&self, request: Request<RaftMessageRequest>) -> Result<Response<RaftMessageResponse>, Status> {
+  let request = request.into_inner();
 
+  //deserealize the bytes into raftmsg 
+  let msg = raft::eraftpb::Message::parse_from_bytes(&request.message)
+    .map_err(|e| Status::internal(format!("decode failed: {}", e)))?;
 
+   //create the raft message 
+   let raft_msg = Msg::Raft(msg);
+   self.tx.send(raft_msg).await.map_err(|e| Status::internal(format!("send failed: {}", e)))?;
 
-    //create the response 
+    //create the response
     let response = RaftMessageResponse {
         success : true
     };
@@ -24,8 +33,20 @@ impl NodeComm for NodeCommServer {
     Ok(Response::new(response))
  }
 
-  async fn forward_proposal(&self , request: Request<ForwardProposalRequest> ) -> Result<Response<ForwardProposalResponse>, Status> {
 
+ //send the proosal 
+  async fn forward_proposal(&self , request: Request<ForwardProposalRequest> ) -> Result<Response<ForwardProposalResponse>, Status> {
+  
+  let request = request.into_inner();
+ 
+   //create the proposal 
+   let proposal_data = raft::eraftpb::Message::parse_from_bytes(&request.proposal_data)
+    .map_err(|e| Status::internal(format!("decode failed: {}", e)))?;
+
+    let raft_msg = Msg::Raft(proposal_data);
+    self.tx.send(raft_msg).await.map_err(|e| Status::internal(format!("send failed: {}", e)))?;
+
+    //create the response 
     let response = ForwardProposalResponse {
         success : true,
         message : "Proposal forwarded successfully".to_string()
