44e2774c407d21917c5228ff34fe1d9d1ab5be4e
its-YogeshChandra <pandittheroyal@gmail.com>
2026-08-14 23:07:17 +0530

    initalized shard controller

--- shardcontroller/src/main.rs
+++ shardcontroller/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
+use tokio::{self, main};
+
+//helper function
+fn parse_env_arguments() {
+    //take the arguments from the env file and parse them
+}
+
+#[main]
+async fn main() {
+    parse_env_arguments();
     println!("Hello, world!");
 }
