0cdbc9aee44998e7fae2551a32f3e4e7ac700ee2
its-YogeshChandra <pandittheroyal@gmail.com>
2026-08-19 05:09:20 +0530

    feat: adding parse env arguments feature

--- clientsdk/src/main.rs
+++ clientsdk/src/main.rs
@@ -1,4 +1,6 @@
+use std::env;
 use tokio;
+use tonic::server;
 mod client;
 mod shard_controller;
 
@@ -7,5 +9,16 @@ pub mod storage_proto {
     tonic::include_proto!("storage_system");
 }
 
+//helper function
+fn parse_env_arguments() {
+    let server_address_vec: Vec<String> = vec![];
+    for i in 0..5 {
+        let env_var = format!("Server{}address", i);
+        println!("env var is : {}", env_var)
+    }
+}
+
 #[tokio::main]
-async fn main() {}
+async fn main() {
+    parse_env_arguments();
+}
